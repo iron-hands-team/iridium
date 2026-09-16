688f22e819cf362b8d05a441cb688694090d33d3
tonymac129 <tonyhsu129@gmail.com>
2026-09-15 22:54:43 -0400

    feat: add login form

--- frontend/components/layout/nav.tsx
+++ frontend/components/layout/nav.tsx
@@ -1,11 +1,13 @@
 import NavTheme from "./nav-theme";
+import Link from "next/link";
 
 function Nav() {
   return (
-    <nav >
-      Iridium
-      <NavTheme/>
-      
+    <nav className="border-b-1 border-zinc-800 flex items-center gap-x-5 py-3 px-50 relative">
+      <Link href="/" className="text-xl font-bold text-black dark:text-white">
+        Iridium
+      </Link>
+      <NavTheme />
     </nav>
   );
 }
--- frontend/components/ui/btn.tsx
+++ frontend/components/ui/btn.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-type ButtonProps = {
-    onClick: () => void;
-    children: React.ReactNode;
-};
+interface ButtonProps {
+  text: string;
+  onclick?: () => void;
+  primary?: boolean;
+}
 
+function Btn({ text, onclick, primary }: ButtonProps) {
+  const btnStyles = `${primary ? "bg-zinc-950 dark:bg-zinc-300 hover:bg-zinc-900 dark:hover:bg-zinc-300/85 text-zinc-300 dark:text-zinc-950" : "hover:bg-zinc-300/85 dark:hover:bg-zinc-900/50"} border-1 border-zinc-800 py-2 cursor-pointer w-full font-bold`;
 
-function Btn({onClick , children} : ButtonProps) {
-  return <button 
-  style={{backgroundColor: "#535353", width: "120px", height: "30px", borderRadius: "10px"
-  }}
-  onClick={onClick}
-  >
-    {children}
-  </button>;
+  return (
+    <button className={btnStyles} onClick={onclick}>
+      {text}
+    </button>
+  );
 }
 
 export default Btn;
